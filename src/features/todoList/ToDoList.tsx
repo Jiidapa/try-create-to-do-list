30296be0403fd7245336123fc003d0f2347395aa
jidapa <jidapa.p@kkumail.com>
2021-04-07 00:40:00 +0700

    [💄] add style

--- src/features/todoList/ToDoList.tsx
+++ src/features/todoList/ToDoList.tsx
@@ -7,6 +7,8 @@ import {
   removeTask,
 } from "./toDoListSlice";
 import styles from "./ToDoList.module.css";
+import { ReactComponent as EmptyIcon } from "../../assets/empty-list2.svg";
+import { ReactComponent as AddIcon } from "../../assets/plus.svg";
 
 export default function ToDoList() {
   const [task, setTask] = useState<string>("");
@@ -16,44 +18,64 @@ export default function ToDoList() {
   return (
     <div className={styles.container}>
       <div className={styles.card}>
-        <div>
-          <input
-            type="text"
-            value={task}
-            onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-              setTask(e.target.value)
-            }
-          />
+        <div className={styles.layoutInput}>
+          <div style={{ width: "100%" }}>
+            <input
+              placeholder="Add task"
+              className={styles.inputAddTask}
+              type="text"
+              value={task}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                setTask(e.target.value)
+              }
+            />
+          </div>
+          <button
+            className={styles.addButton}
+            onClick={() => {
+              dispatch(addToDo({ name: task }));
+              setTask("");
+            }}
+          >
+            <AddIcon className={styles.addIcon} />
+          </button>
         </div>
-        <button
-          onClick={() => {
-            dispatch(addToDo({ name: task }));
-            setTask("");
-          }}
-        >
-          add
-        </button>
-        <div>
-          <div>Results:</div>
-          {list.map((item: any, i: number) => (
-            <div key={i}>
-              {item.name}
-              {item.finish.toString()}
-              <button
-                onClick={() => dispatch(updateTask({ index: i }))}
-                type="button"
-              >
-                done
-              </button>
+        <div className={styles.content}>
+          {list.length > 0 ? (
+            <>
+              {list.map((item: any, i: number) => (
+                <div key={i} className={styles.layoutTask}>
+                  <button
+                    className={styles.buttonTask}
+                    onClick={() => dispatch(updateTask({ index: i }))}
+                    type="button"
+                  >
+                    {item.finish ? "✅" : "⭕️"}&nbsp;&nbsp;
+                    {item.finish ? (
+                      <s className={styles.textFinish}> {item.name}</s>
+                    ) : (
+                      <span> {item.name}</span>
+                    )}{" "}
+                  </button>
 
-              <button
-                onClick={() => dispatch(removeTask({ index: i }))}
-                type="button"
-              >
-                remove
-              </button>
+                  <button
+                    className={styles.buttonDelete}
+                    onClick={() => dispatch(removeTask({ index: i }))}
+                    type="button"
+                  >
+                    🗑
+                  </button>
+                </div>
+              ))}
+            </>
+          ) : (
+            <div className={styles.layoutEmptyList}>
+              <div className={styles.emptyText}>Your list is empty</div>
+              <div>
+                <EmptyIcon className={styles.emptyListIcon} />
+              </div>
             </div>
-          ))}
+          )}
         </div>
       </div>
     </div>
